9aab384149c5e2ce4695a44e39bf83c781b7c905
neeyooo <148869922+neeyooo@users.noreply.github.com>
2024-07-30 11:34:51 +0700

    harusnya sih udh fix bug nya

--- script-js.js
+++ script-js.js
@@ -1,3 +1,45 @@
+// Fungsi untuk menunggu elemen dengan XPath sampai tersedia
+function waitForElementByXPath(xpath, timeout = 10000) {
+    return new Promise((resolve, reject) => {
+        const startTime = Date.now();
+
+        function check() {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                setTimeout(() => {
+                    resolve(element);
+                }, 3); // Menunggu 5 ms setelah elemen ditemukan
+            } else if (Date.now() - startTime >= timeout) {
+                reject(new Error(`Element with XPath ${xpath} not found within ${timeout} ms`));
+            } else {
+                requestAnimationFrame(check);
+            }
+        }
+
+        check();
+    });
+}
+// Fungsi untuk menunggu elemen dengan ID sampai tersedia
+function waitForElementById(id, timeout = 10000) {
+    return new Promise((resolve, reject) => {
+        const startTime = Date.now();
+
+        function check() {
+            const element = document.getElementById(id);
+            if (element) {
+                setTimeout(() => {
+                    resolve(element);
+                }, 3); // Menunggu 5 ms setelah elemen ditemukan
+            } else if (Date.now() - startTime >= timeout) {
+                reject(new Error(`Element with ID ${id} not found within ${timeout} ms`));
+            } else {
+                requestAnimationFrame(check);
+            }
+        }
+
+        check();
+    });
+}
 // Fungsi untuk mendapatkan elemen berdasarkan XPath
 function getElementsByXPath(xpath, parent) {
     let results = [];
@@ -116,105 +158,133 @@ async function getNumberOfQuestions(xpathTemplate) {
     return X - 2; // Kembalikan jumlah pertanyaan terakhir yang valid
 }
 
-
-
 // Fungsi utama yang menjalankan iterasi
 async function main() {
+    var xpath_submit = '//*[@id="submit"]'
     // Cari nilai X maksimal
+    console.log("Mengerjakan survei kepuasan...");
+    waitForElementByXPath('//*[@id="__show_instrumen"]/div[3]/div[1]/div/div');
     let maxXPathTemplate = '//*[@id="__show_instrumen"]/div[3]/div[${X}]/div/div';
     let maxX = findMaxX(maxXPathTemplate);
     console.log(`Jumlah Soal: ${maxX}`);
 
     // Iterasi loop dari 1 hingga nilai X maksimal
     for (var i = 1; i <= maxX; i++) {
-        if (i != 1) {
-            console.log("\n");
-        }
         // XPath yang bervariasi
         var xpath = '//*[@id="__show_instrumen"]/div[3]/div[' + i + ']/div/div/button';
-        console.log("Mengerjakan Soal ke " + i);
+        var xpath_ifelse = getElementsByXPath(xpath);
+        var buttonElement = xpath_ifelse.length > 0 ? xpath_ifelse[0] : null;
 
-        // Mengklik elemen utama
-        await clickElementByXPath(xpath);
-        await delay(100);
-
-        // Mendapatkan teks dari elemen card-text
-        var cardTextXPath = '//*[@id="__show_instrumen"]/div[3]/div[' + i + ']/div/div/p';
-        var jumlahPertanyaan = await getNumberFromXPath(cardTextXPath);
-        console.log('Akan menyelesaikan ' + jumlahPertanyaan + ' pertanyaan');
-
-        for (var j = 1; j <= jumlahPertanyaan; j++) {
-            // XPath untuk menemukan semua radio button di dalam elemen yang ditentukan
-            var radioXPath = '//*[@id="post_survei"]/div[' + j + ']/div//input[@type="radio"]';
-
-            // Mendapatkan ID dari semua elemen radio
-            var radioIds = getRadioIdsByXPath(radioXPath);
-
-            // Memilih salah satu ID secara acak
-            var randomId = getRandomElement(radioIds);
-
-            // Mengklik elemen dengan ID yang dipilih
-            try {
-                document.getElementById(randomId).click();
-            } catch (error) {
-
+        if(buttonElement && buttonElement.textContent === 'Selesai'){
+            console.log('Survei sudah selesai sebelumnya.')
+        }else{
+            if (i != 1) {
+                console.log("\n");
             }
-            await delay(20);
-        }
-        console.log("Selesai mengerjakan soal ke " + i);
+            console.log("Mengerjakan Soal ke " + i);
 
-        // Klik tombol submit
-        await clickElementByXPath('//*[@id="submit" and @type="submit"]');
-        await delay(50);
+            // Mengklik elemen utama
+            await waitForElementByXPath(xpath);
+            await clickElementByXPath(xpath);
+    
+            // Mendapatkan teks dari elemen card-text
+            var cardTextXPath = '//*[@id="__show_instrumen"]/div[3]/div[' + i + ']/div/div/p';
+            await waitForElementByXPath(cardTextXPath);
+            var jumlahPertanyaan = await getNumberFromXPath(cardTextXPath);
+            console.log('Akan menyelesaikan ' + jumlahPertanyaan + ' pertanyaan');
+    
+            for (var j = 1; j <= jumlahPertanyaan; j++) {
+                // XPath untuk menemukan semua radio button di dalam elemen yang ditentukan
+                var radioXPath = '//*[@id="post_survei"]/div[' + j + ']/div//input[@type="radio"]';
+    
+                // Mendapatkan ID dari semua elemen radio
+               await waitForElementByXPath(radioXPath);
+                var radioIds = getRadioIdsByXPath(radioXPath);
+    
+                // Memilih salah satu ID secara acak
+                var randomId = getRandomElement(radioIds);
+    
+                // Mengklik elemen dengan ID yang dipilih
+                try {
+                    await waitForElementById(randomId);
+                    document.getElementById(randomId).click();
+                } catch (error) {
+    
+                }
+                await delay(20);
+            }
+            console.log("Selesai mengerjakan soal ke " + i);
+    
+            // Klik tombol submit
+            await waitForElementByXPath(xpath_submit);
+            await clickElementByXPath(xpath_submit);
+    
+        }
+        
     }
-    console.log("Selesai mengerjakan survei kepuasan\n\n");
+    console.log("Selesai mengerjakan survei kepuasan");
+    console.log("");
+    console.log("");
+    await delay(100);
 
     // Mengerjakan survei dosen
     console.log("Mengerjakan survei dosen")
     maxXPathTemplate = '//*[@id="__show_instrumen"]/div[5]/div[${X}]/div/div';
+    await waitForElementByXPath('//*[@id="__show_instrumen"]/div[5]/div[1]/div/div');
     maxX = findMaxX(maxXPathTemplate);
     console.log(`Jumlah Soal: ${maxX}`);
     for (var i = 1; i <= maxX; i++) {
-        if (i != 1) {
-            console.log("\n");
-        }
+
         // XPath yang bervariasi
         var xpath = '//*[@id="__show_instrumen"]/div[5]/div[' + i + ']/div/div/button';
-        console.log("Mengerjakan Soal ke " + i);
-
-        // Mengklik elemen utama
-        await clickElementByXPath(xpath);
-        await delay(500);
-
-        // Mendapatkan teks dari elemen card-text
-        var cardTextXPath = '//*[@id="post_survei"]/div[${X}]/div';
-        var jumlahPertanyaan = await getNumberOfQuestions(cardTextXPath);
-        console.log('Akan menyelesaikan ' + jumlahPertanyaan + ' pertanyaan');
-
-        for (var j = 1; j <= jumlahPertanyaan; j++) {
-            // XPath untuk menemukan semua radio button di dalam elemen yang ditentukan
-            var radioXPath = '//*[@id="post_survei"]/div[' + j + ']/div//input[@type="radio"]';
-
-            // Mendapatkan ID dari semua elemen radio
-            var radioIds = getRadioIdsByXPath(radioXPath);
-
-            // Memilih salah satu ID secara acak
-            var randomId = getRandomElement(radioIds);
-
-            // Mengklik elemen dengan ID yang dipilih
-            try {
-                document.getElementById(randomId).click();
-            } catch (error) {
-
+        var xpath_ifelse = getElementsByXPath(xpath);
+        var buttonElement = xpath_ifelse.length > 0 ? xpath_ifelse[0] : null;
+        if(buttonElement && buttonElement.textContent === 'Selesai'){
+            console.log('Survei sudah selesai sebelumnya.');
+        }else{
+            if (i != 1) {
+                console.log("\n");
             }
-            await delay(50);
-        }
-        console.log("Selesai mengerjakan soal ke " + i);
+            console.log("Mengerjakan Soal ke " + i);
 
-        // Klik tombol submit
-        await clickElementByXPath('//*[@id="submit"]');
-    }
-    console.log("Selesai mengerjakan survei dosen");
+            // Mengklik elemen utama
+            await waitForElementByXPath('//*[@id="__show_instrumen"]/div[5]/div[1]/div/div/button');
+            await clickElementByXPath(xpath);
+            await delay(10)
+    
+            // Mendapatkan teks dari elemen card-text
+            var cardTextXPath = '//*[@id="post_survei"]/div[${X}]/div';
+            await waitForElementByXPath('//*[@id="post_survei"]/div[20]/div');
+            var jumlahPertanyaan = await getNumberOfQuestions(cardTextXPath);
+            console.log('Akan menyelesaikan ' + jumlahPertanyaan + ' pertanyaan');
+            var element = document.getElementById(randomId);
+            for (var k = 0; k < 2; k++) {
+                for (var j = 1; j <= jumlahPertanyaan; j++) {
+                    // XPath untuk menemukan semua radio button di dalam elemen yang ditentukan
+                    var radioXPath = '//*[@id="post_survei"]/div[' + j + ']/div//input[@type="radio"]';
+            
+                    // Mendapatkan ID dari semua elemen radio
+                    await waitForElementByXPath('//*[@id="post_survei"]/div[' + j + ']/div//input[@type="radio"]');
+                    var radioIds = getRadioIdsByXPath(radioXPath);
+            
+                    // Memilih salah satu ID secara acak
+                    var randomId = getRandomElement(radioIds);
+                    element = document.getElementById(randomId);
+                    if (!element.checked) {
+                        await waitForElementById(randomId);
+                        document.getElementById(randomId).click();
+                    }
+                }
+            }
+            
+            console.log("Selesai mengerjakan soal ke " + i);
+    
+            // Klik tombol submit
+            await waitForElementByXPath(xpath_submit);
+            await clickElementByXPath(xpath_submit);
+            }
+        }
+        console.log("Selesai mengerjakan survei dosen");
 }
 
 // Menjalankan fungsi utama
